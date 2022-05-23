141b2268b79442910fd933f13269b0eda97c5c45
Kirat <kiratsingh0125@gmail.com>
2022-05-23 10:23:37 +0930

    Initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+function Explore(){
+    location.href = 'home.html';
+}
+
+function logoClick(){
+    location.href = 'index.html';
+}
+
+function homePage() {
+    location.href = 'home.html';
+}
+
+function aboutPage() {
+    console.log("check if here")
+    location.href = 'aboutus.html';
+}
+
+function eventPage() {
+    location.href = 'events.html';
+}
+
+function logPage() {
+    location.href = 'login.html';
+}
+
+function signPage() {
+    location.href = 'sign_up.html';
+}
